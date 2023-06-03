06037f264cf9544f3fadd37870062c69c58551ec
oshadiVindya <vindyagodage@gmail.com>
2023-06-03 22:48:26 +0530

    Initial commit

---
+++ spring_boot/movies/src/main/java/devOsh/movies/MoviesApplication.java
@@ -0,0 +1,17 @@
+package devOsh.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+
+@SpringBootApplication
+@Import(CorsConfig.class)
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+		
+	}
+
+}
